de2aa492d20101f22c2561b7498e8793bf5b52d7
forgi <forgi@Plamen.Stoev>
2016-08-03 09:13:47 +0300

    initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+# Use single quotes instead of double quotes to make it work with special-character passwords
+PASSWORD='password'
+PROJECTFOLDER='example1'
+
+# create project folder
+sudo mkdir "/var/www/html/${PROJECTFOLDER}"
+
+# update / upgrade
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# install apache 2.5 and php 5.5
+sudo apt-get install -y apache2
+sudo apt-get install -y php5
+
+# install mysql and give password to installer
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+sudo apt-get -y install mysql-server
+sudo apt-get install php5-mysql
+
+# install phpmyadmin and give password(s) to installer
+# for simplicity I'm using the same password for mysql and phpmyadmin
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+sudo apt-get -y install phpmyadmin
+
+# setup hosts file
+VHOST=$(cat <<EOF
+<VirtualHost *:80>
+    DocumentRoot "/var/www/html/${PROJECTFOLDER}/root"
+    <Directory "/var/www/html/${PROJECTFOLDER}/root">
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF
+)
+echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+
+# enable mod_rewrite
+sudo a2enmod rewrite
+
+# restart apache
+service apache2 restart
+
+# install git
+sudo apt-get -y install git
+
+# install Composer
+curl -s https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+
+sudo apt-get install -y libpcre3-dev
+
+
+sudo apt-get update
+sudo apt-get install -y php5-dev 
+
+git clone -b 2.1.x https://github.com/phalcon/cphalcon.git
+cd cphalcon/ext
+sudo ./install
+sudo echo "extension=phalcon.so" >> /etc/php5/mods-available/phalcon.ini
+sudo php5enmod phalcon
+
+sudo apt-get install -y php5-memcached memcached
+sudo service memcached restart
+
+ sudo service apache2 restart
+
